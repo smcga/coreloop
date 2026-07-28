ef4f571fbe2529f161d2855f6b3d0d6a27ecce00
smcga <s_mcgagh@hotmail.com>
2026-07-28 21:12:22 +0100

    Decompose core run engine

---
+++ packages/core/src/run/state.ts
@@ -0,0 +1,19 @@
+/** Internal domain entry point; public consumers import these from `engine`. */
+export type {
+  CompletedReward,
+  ContentInstance,
+  EncounterBrief,
+  EncounterReport,
+  EncounterRequirements,
+  Inventory,
+  ObjectiveRequirement,
+  PendingAcquisition,
+  PendingReward,
+  RewardOption,
+  RunEffectState,
+  RunPhase,
+  RunState,
+  ScoreLine,
+  ShopOffer,
+  ShopState,
+} from "./reducer";
